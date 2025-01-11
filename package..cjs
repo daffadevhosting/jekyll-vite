15b6de9900632e04957af9673b36200aabaf27b0
daffadevhosting <endsae83@gmail.com>
2025-01-11 12:17:46 +0700

    add gulpfile

---
+++ package..cjs
@@ -0,0 +1,12 @@
+{
+  "private": true,
+  "type": "module",
+  "devDependencies": {
+    "vite": "^5.0.0",
+    "vite-plugin-ruby": "^5.1.0",
+    "gulp": "^4.0.2",
+    "gulp-clean-css": "^4.3.0",
+    "gulp-sass": "^5.1.0",
+    "sass": "^1.58.3"
+  }
+}
